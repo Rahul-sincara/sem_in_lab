bd8365459626f8386f95815afd6a688fdb0a93f8
Rahul-sincara <2400032551@kluniversity.in>
2025-10-17 10:51:00 +0530

    Create UserCard component with user details

--- UserCard.js
+++ UserCard.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import './UserCard.css';
+
+function UserCard({ name, age, bio, image }) {
+  return (
+    <div className="user-card">
+      <img src={image} alt={name} className="user-image" />
+      <h2 className="user-name">{name}</h2>
+      <p className="user-age">Age: {age}</p>
+      <p className="user-bio">{bio}</p>
+    </div>
+  );
+}
+
+export default UserCard;
 
